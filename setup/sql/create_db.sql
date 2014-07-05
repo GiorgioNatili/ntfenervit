1781eb18c169ee1da4455e499c46ccfa4e341b0a
Marcos Lin <linm.linmair@ebacimail.com>
2014-07-05 16:21:00 +0200

    Commented out the dropping of the database and added create if not exists

--- setup/sql/create_db.sql
+++ setup/sql/create_db.sql
@@ -1,7 +1,7 @@
-drop database if exists yellowpagedb;
+# drop database if exists yellowpagedb;
 # drop user 'enervit'@localhost;
 
-create database yellowpagedb DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+create database if not exists yellowpagedb DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
 create user 'enervit'@localhost identified by 'enervitdev';
 grant all on yellowpagedb.* to 'enervit'@localhost;
 
