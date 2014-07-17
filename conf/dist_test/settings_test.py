8e51a710ed1c2b373bf269ad135363f3173a5c39
Domenico Nappo <domenico.nappo@gmail.com>
2014-07-18 01:22:26 +0200

    setting_tests updated with survey app setting

--- conf/dist_test/settings_test.py
+++ conf/dist_test/settings_test.py
@@ -199,3 +199,11 @@ HAYSTACK_CONNECTIONS = {
         'PATH': os.path.join(os.path.dirname(__file__), 'whoosh_index'),
     },
 }
+
+#SURVEY app
+#this setting serve to find the threshold date
+# for a survey to be considered as abandoned
+#It's the maximum number of days passed since
+# last access to the survey to be considered
+# as Active (in corso)
+SURVEY_ACTIVE_DAYS = 15
