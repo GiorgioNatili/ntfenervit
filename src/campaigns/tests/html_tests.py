bef79a7176bc1f63edca49fd3cb2b4371a92262f
Marcos Lin <linm.linmair@ebacimail.com>
2014-08-08 19:57:37 +0200

    Code complete for adding event ratio (T409) and conversion rate (T410)
    Summary:
    Fixed spelling for alert message
    
    Added unit test for EventType
    
    Test Plan: Tested using the website and ran the unit test: manage.py test campaigns
    
    Reviewers: giorgio
    
    Subscribers: domenico.nappo
    
    Maniphest Tasks: T409
    
    Differential Revision: http://projects.gnstudio.biz/D25

---
+++ src/campaigns/tests/html_tests.py
@@ -0,0 +1,187 @@
+# -*- coding: utf-8 -*-
+
+"""
+run client tests
+"""
+
+import lxml.html
+from django.test import TestCase
+from django.test.client import Client
+
+from django.contrib.auth.models import User
+
+
+class BaseHTMLTestCase(TestCase):
+    """
+    Base Test Class for HTML TestCases with GUI utilities
+    """
+    @classmethod
+    def setUpClass(cls):
+        cls.client = Client()
+
+    def _get_elem_from_url(self, url):
+        """
+        Private method to return a parsed element from url
+        :param url: URL to retrieve HTML from
+        :return: a parsed element
+        """
+        resp = self.client.get(url)
+        self.assertEqual(resp.status_code, 200, "Expected '%s' to return 200 but got %s" % (url, resp.status_code))
+        doc = lxml.html.fromstring(resp.content)
+
+        # self.assertTrue(doc, "Failed to parse '%s'.  HTML:\n%s" % (url, resp.content))
+        self.assertTrue(doc, "Failed to parse '%s'" % url)
+        return doc
+
+    def _get_elements(self, element, selector):
+        """
+        Get a list of elements from a selector
+        :param element: Parsed element
+        :param selector: Optional selector to apply on element
+        :return: list of parsed elements
+        """
+        result = []
+        if isinstance(element, list):
+            for elem in element:
+                result.extend(elem.cssselect(selector))
+        else:
+            result.extend(element.cssselect(selector))
+        return result
+
+
+    def _get_list_from_element(self, element, selector=None, attr=None):
+        """
+        Private method to convert element to list of strings
+        :param element: Parsed element
+        :param selector: Optional selector to apply on element
+        :param attr: Optional return given attribute instead of html.  Support for 'object.text' that
+                     return elem.text property.
+        :return: list of string
+        """
+        if selector:
+            elem = self._get_elements(element, selector)
+        else:
+            elem = element
+
+        if attr=="object.text":
+            result = [x.text for x in elem]
+        elif attr:
+            result = [x.get(attr) for x in elem]
+        else:
+            result = [lxml.html.tostring(x) for x in elem]
+
+        return result
+
+
+class EventTypeHTMLTestCase(BaseHTMLTestCase):
+    fixtures = ['initial_data', 'users_tests.json', 'contacts_tests.json', 'events_tests.json']
+
+    def setUp(self):
+        self.admin = User.objects.get(pk=1)
+        self.is_logged_in = self.client.login(username=self.admin.username, password="devel02")
+        self.eventType_id_Seminario = 1
+
+
+    def test01_EventType_list(self):
+        '''Check for EventType list view'''
+        doc = self._get_elem_from_url("/admin/campaigns/eventtype/")
+
+        # Make sure header is generated as expected
+        headers = self._get_list_from_element(doc, selector="#dataTables > table > thead > tr > th > div", attr="object.text")
+        expected_headers = ['Descrizione', 'Contatti Lordi', 'Contatti Netti', 'Selezionabile']
+        self.assertEqual(headers, expected_headers)
+
+        # Make sure number of row returned is as expected
+        rows = self._get_elements(doc, selector="#dataTables > table > tbody > tr")
+        self.assertEqual(len(rows), 5, "Expected EventType list to contain 5 entries but got %d" % len(rows))
+
+        # Look for "PORTALE ENS" and "SEMINARIO" rows and check for the data
+        for row in rows:
+            cells = row.getchildren()
+            title = cells[0].text
+            if title == "PORTALE ENS":
+                row_text = self._get_list_from_element(row, attr="object.text")
+                expected_row_text = ['PORTALE ENS', '1', '3,8 %', 'No', None]
+                self.assertEqual(row_text, expected_row_text)
+            elif title == "SEMINARIO":
+                row_text = self._get_list_from_element(row, attr="object.text")
+                expected_row_text = ['SEMINARIO', '40', '20,0 %', u'Sì', None]
+                self.assertEqual(row_text, expected_row_text)
+
+    def test02_EventType_edit(self):
+        '''Test Editing existing EventType'''
+        url = "/admin/campaigns/eventtype/%d" % self.eventType_id_Seminario
+
+        # Check initial data
+        doc = self._get_elem_from_url(url)
+        result = self._get_list_from_element(doc, "form#company_form > div > div.span6 > ul > li > input", attr="value")
+        expected_result = ['SEMINARIO', None, '40.0', '0.2']
+        self.assertEqual(result, expected_result)
+
+        # Make sure update succeeds and redirect expected URL
+        csrf_token=self._get_list_from_element(doc, "form#company_form > input[name='csrfmiddlewaretoken']", attr="value")
+        self.assertEqual(len(csrf_token),1,"Expected to find csrfmiddlewaretoken")
+        post_data = {
+            "description": "SEMINARIO FfRPCZQZI7",
+            "selectable": True,
+            "contact_to_customer": "14.0",
+            "customer_to_sale": "0.4",
+            "csrfmiddlewaretoken": csrf_token[0]
+        }
+
+        resp = self.client.post(url, post_data)
+        self.assertEqual(resp.status_code, 302)
+        self.assertRegexpMatches(resp.get("location"), r'\/admin\/campaigns\/eventtype$')
+
+        # Check updated data
+        doc = self._get_elem_from_url(url)
+        result = self._get_list_from_element(doc, "form#company_form > div > div.span6 > ul > li > input", attr="value")
+        expected_result = [post_data["description"], None, post_data["contact_to_customer"], post_data["customer_to_sale"]]
+        self.assertEqual(result, expected_result)
+
+    def test03_EventType_add(self):
+        '''Test Adding a new EventType'''
+        url = "/admin/campaigns/eventtype/add/"
+
+        # Create post data with the csrf_token
+        doc = self._get_elem_from_url(url)
+        csrf_token=self._get_list_from_element(doc, "form#company_form > input[name='csrfmiddlewaretoken']", attr="value")
+        self.assertEqual(len(csrf_token),1,"Expected to find csrfmiddlewaretoken")
+        post_data = {
+            "description": "TEST tIZc66b7LP",
+            "selectable": False,
+            "contact_to_customer": "66",
+            "customer_to_sale": "0.7",
+            "customer_to_sale_percent": "70,0 %",
+            "csrfmiddlewaretoken": csrf_token[0]
+        }
+
+        # Make sure post works
+        resp = self.client.post(url, post_data)
+        self.assertEqual(resp.status_code, 302)
+        self.assertRegexpMatches(resp.get("location"), r'\/admin\/campaigns\/eventtype$')
+
+        # Check that it has been added
+        doc = self._get_elem_from_url("/admin/campaigns/eventtype/")
+
+        # Make sure number of row returned is as expected
+        rows = self._get_elements(doc, selector="#dataTables > table > tbody > tr")
+        self.assertEqual(len(rows), 6, "Expected EventType list to contain 6 entries but got %d" % len(rows))
+
+        # Look for "PORTALE ENS" and "SEMINARIO" rows and check for the data
+        for row in rows:
+            cells = row.getchildren()
+            title = cells[0].text
+            if title == post_data['description']:
+                row_text = self._get_list_from_element(row, attr="object.text")
+                expected_row_text = [post_data["description"], post_data["contact_to_customer"], post_data["customer_to_sale_percent"], 'No', None]
+                self.assertEqual(row_text, expected_row_text)
+
+    def test04_EventType_dropdown(self):
+        '''Make sure EventType dropdown only shows selectable = True'''
+        url = "/admin/campaigns/event/add/"
+        doc = self._get_elem_from_url(url)
+
+        result = self._get_list_from_element(doc, "form#event_form > div.row-fluid > div.span6 > ul > li > select[name='eventtype'] > option", attr="object.text")
+        result_expected = ['---', 'SEMINARIO', 'ONE-TO-ONE', 'FORMAZIONE A PUNTO VENDITA', 'INFORMAZIONE MEDICA']
+        self.assertItemsEqual(result, result_expected)
