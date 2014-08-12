8b0e6ae1a8bd85b1993cbeaecf0efe1ec81d11c4
Domenico Nappo <domenico.nappo@gmail.com>
2014-08-12 22:18:25 +0200

    complete for test environment
    note: in production it can change

--- release/0.4.0/populate_trainer.py
+++ release/0.4.0/populate_trainer.py
@@ -6,12 +6,35 @@ from campaigns.models import Event
 # events with trainer field filled.
 # to verify in production the ids (should be the same)
 # events = Event.objects.exclude(trainer__isnull=True).exclude(trainer__exact='')
+
+'''
+under test
+21 44
+CIACCI MANILA
+
+22
+TRAINO
+
+23 41
+BACCOLINI FEDERICA
+
+40
+Iader Fabbri
+
+43
+CARNEVALI STEFANO
+
+60
+BARRY SEARS
+'''
 e21 = Event.objects.get(pk=21)
+e22 = Event.objects.get(pk=22)
 e23 = Event.objects.get(pk=23)
 e40 = Event.objects.get(pk=40)
 e41 = Event.objects.get(pk=41)
 e43 = Event.objects.get(pk=43)
 e44 = Event.objects.get(pk=44)
+e60 = Event.objects.get(pk=60)
 
 ciacci = Contact()
 ciacci.type = 'C'
@@ -26,6 +49,18 @@ e21.consultant = ciacci
 e44.consultant = ciacci
 e21.save()
 e44.save()
+
+traino = Contact()
+traino.type = 'C'
+traino.status = 'I'
+traino.name = 'MANILA'
+traino.surname = 'CIACCI'
+traino.email = ''
+traino.code = 'CCCMNL00XX00E999V'
+traino.save()
+e22.consultant = traino
+e22.save()
+
 
 baccolini = Contact()
 baccolini.type = 'C'
@@ -64,3 +99,15 @@ carnevali.save()
 
 e43.consultant = carnevali
 e43.save()
+
+barry = Contact()
+barry.type = 'C'
+barry.status = 'I'
+barry.name = 'BARRY'
+barry.surname = 'SEARS'
+barry.email = ''
+barry.code = 'SRSBRR00XW00E001G'
+barry.save()
+
+e60.consultant = barry
+e60.save()
