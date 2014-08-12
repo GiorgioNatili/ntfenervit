5d8de79deb3a54d43202e48d23734026ef7b2fc3
Domenico Nappo <domenico.nappo@gmail.com>
2014-08-12 22:21:11 +0200

    added src to the path!

--- release/0.4.0/populate_trainer.py
+++ release/0.4.0/populate_trainer.py
@@ -2,7 +2,8 @@ __author__ = 'dominik'
 
 from contacts.models import Contact
 from campaigns.models import Event
-
+import sys
+sys.path.append('./src')
 # events with trainer field filled.
 # to verify in production the ids (should be the same)
 # events = Event.objects.exclude(trainer__isnull=True).exclude(trainer__exact='')
